d0740b389c2689b81dc49e9dfdab930b138be456
hasrakulov?tab=repositories <hasrakulovazimjon00@gmail.com>
2024-07-06 11:30:17 +0500

    Initial commit

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 const TelegramBot = require('node-telegram-bot-api');
 const translate = require('translate-google');
 
-// Telegram bot tokeni
-const TELEGRAM_TOKEN = '6606994240:AAH0m2bhW3aje6gtlXte8dd7ayVk7-G6BZM'; // Bu yerda tokeningizni kiriting
+const TELEGRAM_TOKEN = '6606994240:AAH0m2bhW3aje6gtlXte8dd7ayVk7-G6BZM';
+
 const bot = new TelegramBot(TELEGRAM_TOKEN, { polling: true });
 
 bot.on('polling_error', (error) => {
-  console.log(error);  // Polling errorlarni konsolga chiqarish
+  console.log(error);
 });
 
-// /start komandasi
 bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, 'Salom! So\'z yoki matn yuboring va men uni ingliz va rus tillariga tarjima qilaman.');
+  bot.sendMessage(msg.chat.id, 'Salom bizning telegram botga xush kelibsiz! So\'z yoki matn yuboring va men uni ingliz va rus tillariga tarjima qilaman.');
 });
 
-// Xabarlarni qayta ishlash
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
 
-  // Tarjimalar
   translate(text, { to: 'en' }).then(englishTranslation => {
     translate(text, { to: 'ru' }).then(russianTranslation => {
       const response = `
@@ -30,11 +27,11 @@ bot.on('message', (msg) => {
 
       bot.sendMessage(chatId, response);
     }).catch(err => {
-      console.error('Rus tiliga tarjima qilishda xatolik:', err); // Xatolikni konsolga chiqarish
+      console.error('Rus tiliga tarjima qilishda xatolik:', err); 
       bot.sendMessage(chatId, 'Rus tiliga tarjima qilishda xatolik yuz berdi.');
     });
   }).catch(err => {
-    console.error('Ingliz tiliga tarjima qilishda xatolik:', err); // Xatolikni konsolga chiqarish
+    console.error('Ingliz tiliga tarjima qilishda xatolik:', err);
     bot.sendMessage(chatId, 'Ingliz tiliga tarjima qilishda xatolik yuz berdi.');
   });
 });
